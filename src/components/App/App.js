68d7bba64783a0bba746e514d71aefabf7f2e245
Derek Cooper <derekcooper20@gmail.com>
2022-12-06 11:13:32 -0700

    Adds routes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,23 @@
 import './App.css';
 import React from 'react'
 import ZipcodeSearch from '../ZipcodeSearch/ZipcodeSearch';
+import { SearchItem } from '../SearchItem/SearchItem';
 import Header from '../Header/Header';
+import { Route, Routes } from 'react-router-dom';
+import { Footer } from '../Footer/Footer';
+import { CartItem } from '../CartItem/CartItem';
 
 const App = () => {
 
   return (
     <div className="App">
       <Header />
-      <ZipcodeSearch />
+      <Routes>
+      <Route path='/' element={<ZipcodeSearch />} />
+      <Route path='/search' element={<SearchItem />} />
+      <Route path='/cart' element={<CartItem />}></Route>
+      </Routes>
+      <Footer />
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,20 +1,30 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import headerImage from '../../assetts/headerImage.jpeg'
+import { BsCart4 } from 'react-icons/bs';
 import './Header.css'
 
 const Header = () => {
 
     return (
+
         <header className='header-container'>
-            <img 
+            <img
                 className='header-image'
                 src={headerImage} alt={''}
             />
             <h1>Frugal Foods</h1>
-            <button className='home'>Home</button>
+            <Link to='/' className='home'>Home</Link>
             <button className='list'>Grocery List</button>
-            <button className='search'>Search</button>
+            <Link to='/search' className='search'>Search</Link>
+            <Link to='/cart'>
+                <div className='header-openBasket'>
+                    <BsCart4 size={25} />
+                    <span className='header-basketCount'>5</span>
+                </div>
+            </Link>
         </header>
+
     )
 }
 
