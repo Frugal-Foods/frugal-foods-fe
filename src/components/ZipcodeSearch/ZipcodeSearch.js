6291a0df9dccc73de5efaadae147036c6e070f0b
Nicole Forseth <forseth.nico@gmail.com>
2022-12-05 21:54:10 -0700

    Add favicon and logos to assets

--- src/components/ZipcodeSearch/ZipcodeSearch.js
+++ src/components/ZipcodeSearch/ZipcodeSearch.js
@@ -1,55 +1,40 @@
-import React, {useState} from 'react'
-import { useStores } from '../../hooks/useStores'
+import React, { useState } from 'react'
+import { getStores } from '../../hooks/useStores'
 
 const ZipcodeSearch = () => {
     const [searchValue, setSearchValue] = useState('')
-    // const [stores, setStores] = useState([])
-    const { data, loading, error } = useStores(searchValue)
-    // OR? const { data, loading, error } = useStores('64693')
-    console.log({data, loading, error})
-    // should this go in the submit function bc that's where we want this call to happen, once Find Stores button is clicked the call gets made stores should populate
-    // we need to capture the search value (we are doing this currently)
-        // that searchValue will inform what stores populate 
-    
+    const [stores, setStores] = useState([])
+    const [error, setError] = useState('')
+
     const handleSubmit = (event) => {
         event.preventDefault()
-        // setStores(data.stores)
-        // const newZipcode = {
-        //   searchValue
-        // }
-        // this.props.addUrl(newUrl)
-        // the stores are displaying right now as soon as a matching zipcode is typed in, no need to click Find Stores
-        // if i remember correctly we also need to POST the user? so that any stores selected becomes associated with the user, so maybe have a mutation put on the Find Stores button and the POST can happen then/there?
+        if(searchValue) {
+        const { data } = getStores(searchValue)
+        setStores(data.stores)
         clearInputs();
+        } else {
+            setError('Enter a 5 digit zipcode')
+        }
       }
-      console.log(searchValue)
 
-    const clearInputs = (event) => {
-        event.preventDefault()
+    const clearInputs = () => {
         setSearchValue('')
+        setError('')
     }
-
-    // from rick and morty tutorial I should be getting that data to console.log from line 6 but it remains undefined
-    // if I was getting data back, the Find Stores button could trigger a new view (using <Link /> wrapped around the button) and then display the store that has that zipcode
-    // right now we are successfully capturing the text box input, but we are getting a 404 Error 
-    // data should console.log: {
-        //   "id": "1",
-        //   "name": "Jast, Upton and Barrows",
-        //   "address": "Suite 456 823 Patricia Garden, East Corrina, CT 64693"
-        // }
 
     return (
         <div>
-            <h3>Search Zipcode: </h3>
+            <h3>Find Grocery Stores Near You: </h3>
             <input 
                 type='text'
-                placeholder='Enter Zipcode'
+                placeholder='Enter zipcode'
                 name='zipcode'
                 value={searchValue}
                 onChange={(e) => setSearchValue(e.target.value)}
             />
             <button onClick={(event) => handleSubmit(event)}>Find Stores</button>
-            <section className='store-container'>{data.stores.map(store => {
+            {error && <h4 className="error-message">{error}</h4>}
+            <section className='store-container'>{stores.map(store => {
                 return <div>
                     <h2>Store Name: {store.name}</h2>
                     <h3>Store Address: {store.address}</h3>
