00726fbfee956eb17e914078c37c5d3e218cc0cd
Derek Cooper <derekcooper20@gmail.com>
2022-12-08 13:33:13 -0700

    Adds cypress boilerplate and styles components

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import "./Footer.css";
 
 export const Footer = () => {
   return (
     <div className='footer'>
+      <div>
       <h4>Frugal Foods~2022</h4>
+      </div>
     </div>
   )
 }
