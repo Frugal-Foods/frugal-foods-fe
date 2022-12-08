1c5663079834a99bafb3d7734246ebb9b6a9b4b7
Danielle Sweeny <dsweeny1@gmail.com>
2022-12-08 16:30:04 -0700

    Make circleci happy

--- src/components/CartPage/CartPage.js
+++ src/components/CartPage/CartPage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import CartContainer from "../CartContainer/CartContainer";
 import "./CartPage.css";
-import { useCartItems } from "../../hooks/useStores";
+import { useCartItems } from "../../hooks/getQueries";
 
 const CartPage = () => {
   const { data, loading, error } = useCartItems(1);
