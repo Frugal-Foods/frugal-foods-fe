5b91e75cb1b13e4be543a5bc646d47ee02b2f5e4
Nicole Forseth <forseth.nico@gmail.com>
2022-12-10 12:02:46 -0700

    Update import typo

--- src/components/CartPage/CartPage.js
+++ src/components/CartPage/CartPage.js
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import CartContainer from "../CartContainer/CartContainer";
-import { useCartItems } from "../../hooks/useQueries";
+import { useCartItems } from "../../hooks/getQueries";
 import { UserContext } from "../../context/userContext";
 import "./CartPage.css";
 
