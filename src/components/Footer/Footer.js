651f9434ad9bba572bdbe0e6ebfe66bce10bf8cb
Derek Cooper <derekcooper20@gmail.com>
2022-12-13 21:42:57 -0700

    Adds testing fixtures and footer styling

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,12 +1,11 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
 import "./Footer.css";
 
 export const Footer = () => {
   return (
     <div className='footer'>
       <p>© Frugal Foods 2022</p>
-      <p>Creators GitHubs · <a target="_blank" href='https://github.com/forsethnico' className='creators'>Nicole Forseth</a> · <a target="_blank" href='https://github.com/dsweeny1' className='creators'>Danielle Sweeny</a> · <a target="_blank" href='https://github.com/coopercodex' className='creators'>Derek Cooper</a></p>
+      <p>Creators GitHubs · <a target="_blank" rel="noreferrer" href='https://github.com/forsethnico' className='creators'>Nicole Forseth</a> · <a target="_blank" rel="noreferrer" href='https://github.com/dsweeny1' className='creators'>Danielle Sweeny</a> · <a target="_blank" rel="noreferrer" href='https://github.com/coopercodex' className='creators'>Derek Cooper</a></p>
     </div>
   )
 }
