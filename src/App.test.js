50d669e5b6cbdc166aa716672c03ec16deea2479
Danielle Sweeny <dsweeny1@gmail.com>
2022-12-15 10:43:38 -0700

    Make circleci happy

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,26 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./components/App/App";
+import reportWebVitals from "./reportWebVitals";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import { BrowserRouter } from "react-router-dom";
 
-test('renders learn react link', () => {
-  render(<App />);
+const client = new ApolloClient({
+  uri: "https://frugal-foods-be.fly.dev/graphql",
+  cache: new InMemoryCache(),
+});
+
+test('renders Frugal Foods app', () => {
+  render(
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </BrowserRouter>
+  );
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
