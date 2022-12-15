6faece8fe9d55a2d89d847f9d281133673e04f03
Danielle Sweeny <dsweeny1@gmail.com>
2022-12-15 10:46:01 -0700

    Make circleci happy

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,7 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
 import React from "react";
-import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./components/App/App";
-import reportWebVitals from "./reportWebVitals";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import { BrowserRouter } from "react-router-dom";
 
