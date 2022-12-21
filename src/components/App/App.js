653206e75beb9dc782608523c2646e650b78e69e
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-12-20 19:29:21 -0700

    working feature

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useEffect, useState } from 'react';
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import { UserContext } from "../../context/userContext";
@@ -7,10 +8,25 @@ import { Footer } from "../Footer/Footer";
 import SearchPage from "../SearchPage/SearchPage";
 import StoresPage from "../StoresPage/StoresPage";
 import CartPage from "../CartPage/CartPage";
+import { GET_USER } from "../../hooks/getQueries";
+import { useLazyQuery, useMutation } from "@apollo/client";
 
 const App = () => {
+  const [user, setUser] = useState(0);
+  const [getUser, { loading, error }] = useLazyQuery(GET_USER, {
+    onCompleted: (data) => setUser(data?.user[0].id),
+  });
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const response = await getUser();
+      setUser(parseInt(response.data.user[0].id));
+    };
+    fetchUser();
+  }, []);
+
   return (
-   <UserContext.Provider value={5}>
+   <UserContext.Provider value={user}>
     <div className="App">
       <Header />
       <Routes>
@@ -33,6 +49,7 @@ const App = () => {
       <Footer />
     </div>
     </UserContext.Provider> 
+    
   );
 };
 
